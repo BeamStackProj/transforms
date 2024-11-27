d5c684970776fd35b54e1ed68be7509382add2b7
kennedyuche <kennedy@mavencode.com>
2024-11-27 09:14:38 -0600

    updated beamstack provider impl

--- src/beamstack_provider.py
+++ src/beamstack_provider.py
@@ -6,30 +6,107 @@ import subprocess
 import sys
 import yaml
 import apache_beam as beam
-from apache_beam.transforms import external
 from typing import Any, Iterable, Mapping, Optional, Callable
 from apache_beam.yaml.yaml_provider import ExternalProvider
-from apache_beam.utils import subprocess_server
 import logging
 import importlib.util
+import urllib.request
+from urllib.parse import urlparse
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+class BeamstackProviderPathHandler:
+    def is_local_file(self, path: str) -> bool:
+        """Check if the path is a local file."""
+        return os.path.exists(path)
+
+    def is_github_file(self, path: str) -> bool:
+        """Check if the path is a GitHub file URL."""
+        parsed_url = urlparse(path)
+            
+        if parsed_url.netloc == "raw.githubusercontent.com":
+            return True
+        if parsed_url.netloc == "github.com":
+            return True
+        
+        return False
+
+    def is_gcs_file(self, path: str) -> bool:
+        """Check if the path is a Google Cloud Storage URL."""
+        return path.startswith('gs://')
+
+    def handle_local_file(self, path: str):
+        """Handle local file or directory path."""
+        logger.info(f"Pulling transforms yaml from: {path}")
+        if not os.path.exists(path):
+            raise FileNotFoundError(f"The local path '{path}' does not exist.")
+        return path
+
+    def handle_github_file(self, file_url: str, target_dir: str):
+        """Download file from a public GitHub repository to the target path."""
+        logger.info(f"Pulling transforms yaml from GitHub url: {file_url}")
+
+        if "github.com" in file_url and "/blob/" in file_url:
+            file_url = file_url.replace("github.com", "raw.githubusercontent.com").replace("blob/", "")
+        
+        os.makedirs(target_dir, exist_ok=True)
+
+        file_name = os.path.basename(file_url)
+        local_file_path = os.path.join(target_dir, file_name)
+        
+        try:
+            logger.info(f"Downloading {file_name} to {local_file_path}")
+            urllib.request.urlretrieve(file_url, local_file_path)
+        except Exception as e:
+            logger.info(f"Error occured during file download: {e}")
+
+        return local_file_path
+
+    def handle_gcs_file(self, gcs_path: str, target_dir: str):
+        """Download files from a public GCS bucket to a target path."""
+        logger.info(f"Pulling transforms yaml from GCS path: {gcs_path}")
+
+        gcs_path = gcs_path[len("gs://"):]
+        bucket_name, _, object_name = gcs_path.partition('/')
+        public_url = f"https://storage.googleapis.com/{bucket_name}/{object_name}"
+        
+        os.makedirs(target_dir, exist_ok=True)
+        local_file_path = os.path.join(target_dir, os.path.basename(object_name))
+        
+        try:
+            logger.info(f"Downloading {os.path.basename(object_name)} to {target_dir}")
+            urllib.request.urlretrieve(public_url, local_file_path)
+        except Exception as e:
+            logger.info(f"Error downloading file from {public_url}: {e}")
+        
+        return local_file_path
+
 @ExternalProvider.register_provider_type('BeamstackTransform')
 def BeamstackTransform(urns, path):
-    with open(path, 'r') as f:
+    target_dir = '/tmp/beamstack_transforms'
+    
+    path_handler = BeamstackProviderPathHandler()
+
+    if path_handler.is_local_file(path):
+        transform_yaml_path = path_handler.handle_local_file(path)
+    elif path_handler.is_github_file(path):
+        transform_yaml_path = path_handler.handle_github_file(path, target_dir)
+    elif path_handler.is_gcs_file(path):
+        transform_yaml_path = path_handler.handle_gcs_file(path, target_dir)
+    else:
+        raise ValueError(f"Unsupported path type: {path}")
+    
+    with open(transform_yaml_path, 'r') as f:
         transform_yaml = yaml.safe_load(f)
     
     config = {
         'urns': urns,
-        'yaml_path': path,
-        'dependencies': transform_yaml.get('dependencies', []),
-        'runner': transform_yaml.get('runner', [])
+        'yaml_path': transform_yaml_path,
+        'dependencies': transform_yaml.get('dependencies', [])
     }
     
     return BeamstackTransformProvider(urns, config)
-
 
 class BeamstackTransformProvider(ExternalProvider):
     def __init__(self, urns, config):
@@ -91,11 +168,16 @@ class BeamstackTransformProvider(ExternalProvider):
         try:
             logger.info(f"Loading transform class for: {transform_name}")
             
-            spec = importlib.util.spec_from_file_location(f"{transform_map[transform_name]}.py", 
-                                                          os.path.join(self._service._venv_path(), 
-                                                                f"{transform_map[transform_name]}.py"))
-            
+            spec = importlib.util.spec_from_file_location(
+                f"{transform_map[transform_name]}.py",
+                os.path.join(self._service._venv_path(), f"{transform_map[transform_name]}.py")
+            )
+            if spec is None:
+                logger.error(f"Specification for module '{transform_map[transform_name]}' could not be found.")
+                return None
+
             module = importlib.util.module_from_spec(spec)
+            sys.path.insert(0, os.path.dirname(spec.origin))
             spec.loader.exec_module(module)
             transform_class = getattr(module, transform_name)
             logger.info(f"Loaded transform class: {transform_class}")
@@ -103,12 +185,6 @@ class BeamstackTransformProvider(ExternalProvider):
         except Exception as e:
             logger.error(f"Failed to load transform {transform_name}: {e}")
             raise e
-
-    @classmethod
-    def provider_from_spec(cls, spec):
-        urns = spec['transforms']
-        config = spec['config']
-        return cls(urns, config)
 
 
 class BeamstackExpansionService:
@@ -139,7 +215,8 @@ class BeamstackExpansionService:
 
     def _write_source_file(self, src_name, code):
         """Writes decoded code to file for each source."""
-        file_path = os.path.join(self._venv_path(), src_name)
+        venv = self._venv_path()
+        file_path = os.path.join(venv, src_name)
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
         with open(file_path, 'w') as f:
             f.write(code)
@@ -151,45 +228,28 @@ class BeamstackExpansionService:
         venv = os.path.join(self.VENV_CACHE, venv_hash)
         
         if not os.path.exists(venv):
+            subprocess.run([self.base_python, '-m', 'venv', venv], check=True)
+        
+        site_packages_path = os.path.join(venv, 'lib', f'python{sys.version_info.major}.{sys.version_info.minor}', 'site-packages')
+        if site_packages_path not in sys.path:
+            sys.path.insert(0, site_packages_path)
+
+        venv_pip = os.path.join(venv, 'bin', 'pip')
+        
+        if os.path.exists(venv_pip):
             installed_packages = subprocess.check_output(
-                [os.path.join(venv, 'bin', 'pip'), 'list', '--format=freeze']
+                [venv_pip, 'list', '--format=freeze']
             ).decode('utf-8').splitlines()
             
             installed_packages_set = {pkg.split('==')[0] for pkg in installed_packages}
             
-            subprocess.run([self.base_python, '-m', 'venv', venv], check=True)
-            venv_pip = os.path.join(venv, 'bin', 'pip')
-            
             for package in self._packages:
                 if package not in installed_packages_set:
                     logger.info(f"Installing package: {package}")
-                    subprocess.run([venv_pip, 'install'] + self._packages, check=True)
+                    subprocess.run([venv_pip, 'install', package], check=True)
                 else:
                     logger.info(f"Package '{package}' is already installed; skipping installation.")
+        else:
+            raise FileNotFoundError(f"Could not find pip at expected location: {venv_pip}")
         
         return venv
-
-    # def __enter__(self):
-    #     venv = self._venv_path
-    #     self._service_provider = subprocess_server.SubprocessServer(
-    #         external.ExpansionAndArtifactRetrievalStub,
-    #         [
-    #             os.path.join(venv, 'bin', 'python3'),
-    #             '-m',
-    #             'apache_beam.runners.portability.expansion_service_main',
-    #             '--port',
-    #             '{{PORT}}',
-    #             '--fully_qualified_name_glob=*',
-    #             '--pickle_library=cloudpickle',
-    #         ]
-    #     )
-    #     self._service = self._service_provider.__enter__()
-    #     return self._service
-
-    # def __exit__(self, *args):
-    #     self._service_provider.__exit__(*args)
-    #     self._service = None
-        
-    #     if os.path.exists(self._venv_path()):
-    #         subprocess.run(['rm', '-rf', self._venv_path()])
-    #         logger.info("Cleaned up virtual environment after pipeline run.")
--- src/transforms_garden/utils.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-class LogHandler:
-    def logger(self):
-        logging.basicConfig(level=logging.INFO)
-        logger = logging.getLogger(__name__)
-
-        return logger
